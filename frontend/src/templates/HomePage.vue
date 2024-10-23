b3df7613024f247250b287d662167febf29a7cb8
Felipe-dSantos <lipehh18@gmail.com>
2024-10-23 03:19:52 -0300

    Update HomePage.vue

--- frontend/src/templates/HomePage.vue
+++ frontend/src/templates/HomePage.vue
@@ -1,4 +1,5 @@
 <template>
+  <ToastComponent v-if="showToast" :message="toastMessage" :type="toastType" />
   <div class="container mx-auto md:px-20">
     <h1 class="text-2xl sm:text-3xl md:text-4xl text-center mt-5 mb-5 font-bold">
       Bem vindo (Profissional de Saude)
@@ -59,14 +60,67 @@
                 @click="openModal(assessment)">
                 Visualizar
               </button>
-                <button class="bg-yellow-500 text-white py-1 px-2 rounded disabled">Editar</button>
-                <button class="bg-red-500 text-white py-1 px-2 rounded disabled">Excluir</button>
+              <button class="bg-yellow-500 text-white py-1 px-2 rounded " @click="editpage">Editar</button>
+                <button class="bg-red-500 text-white py-1 px-2 rounded " 
+                        aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-danger-alert"
+                        data-hs-overlay="#hs-danger-alert"
+                  @click="openDeleteModal(assessment)" >Excluir</button>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
+
+    <!-- Modal de confirmação de exclusão -->
+    <div id="hs-danger-alert"
+        class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto" role="dialog"
+        tabindex="-1" aria-labelledby="hs-danger-alert-label">
+        <div
+          class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
+          <div
+            class="relative flex flex-col bg-white border shadow-sm rounded-xl overflow-hidden dark:bg-neutral-900 dark:border-neutral-800">
+            <div class="absolute top-2 end-2">
+              <button type="button"
+                class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
+                aria-label="Close" data-hs-overlay="#hs-danger-alert" @click="cancelDelete">
+                <span class="sr-only">Close</span>
+                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              </button>
+            </div>
+            <div class="p-4 sm:p-10 overflow-y-auto bg-gray-50">
+              <div class="flex gap-x-4 md:gap-x-7">
+                <span
+                  class="shrink-0 inline-flex justify-center items-center size-[46px] sm:w-[62px] sm:h-[62px] rounded-full border-4 border-red-50 bg-red-100 text-red-500">
+                  <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" viewBox="0 0 16 16">
+                    <path
+                      d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                  </svg>
+                </span>
+                <div class="grow">
+                  <h3 id="hs-danger-alert-label" class="mb-2 text-xl font-bold text-gray-800">Excluir Avaliação</h3>
+                  <p class="text-gray-500">Tem certeza que deseja excluir esta avaliação? Essa ação não poderá ser
+                    desfeita.</p>
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-end items-center gap-x-2 py-3 px-4 bg-gray-50 border-t">
+              <button @click="cancelDelete" type="button"
+                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50"
+                aria-label="Close" data-hs-overlay="#hs-danger-alert">Cancelar</button>
+              <button @click="confirmDeleteAssessment" type="button"
+                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-red-500 text-white hover:bg-red-600 disabled:opacity-50 disabled:pointer-events-none"
+                aria-label="Close" data-hs-overlay="#hs-danger-alert">Excluir</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
     <!-- Modal de visualização -->
     <div id="hs-large-modal"
@@ -144,7 +198,11 @@
 </template>
 
 <script>
+import ToastComponent from '../components/ToastNotification.vue';
 export default {
+  components: {
+    ToastComponent,
+  },
   data() {
     return {
       assessments: [
@@ -269,10 +327,49 @@ export default {
           }
         }
       ],
-      currentAssessment: null
+      toastMessage: '',
+      toastType: 'success',
+      showModal: false, // Adicione esta linh
+      currentAssessment: null,
+      assessmentToDelete: null,
+      deletedAssessments: []
     };
   },
   methods: {
+    AssessmentPage(id) {
+      this.$router.push(`/AssessmentPage/${id}`);
+    },
+    editpage() {
+      this.$router.push('/EditAssessment');
+    },
+    EditaData() {
+
+    }, 
+    showToastMessage(message, type) {
+      this.toastMessage = message;
+      this.toastType = type;
+      this.showToast = false;
+      this.$nextTick(() => {
+        this.showToast = true;
+      });
+    },
+    openDeleteModal(assessment) {
+      this.assessmentToDelete = assessment;
+      this.showModal = true;
+    },
+    confirmDeleteAssessment() {
+      if (this.assessmentToDelete) {
+        this.deletedAssessments.push(this.assessmentToDelete.id_assessment);
+        // Remover a avaliação da lista
+        this.assessments = this.assessments.filter(assessment => assessment.id_assessment !== this.assessmentToDelete.id_assessment);
+        this.showToastMessage('Avaliação excluída com sucesso', 'success');
+        this.cancelDelete();
+      }
+    },
+    cancelDelete() {
+      this.assessmentToDelete = null;
+      this.showModal = false;
+    },
     openModal(assessment) {
       this.currentAssessment = assessment; // Define a avaliação selecionada
     },
